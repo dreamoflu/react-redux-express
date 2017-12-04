0883a7d118b6a4c6a7a2758a1ae372ca2bff4a23
Lyj-js <1148869219@qq.com>
2017-12-04 23:19:10 +0800

    最新

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 import App from './app'
-import { createStore, applyMiddleware } from 'redux'
+import { createStore,applyMiddleware,compose } from 'redux'
 import thunk from 'redux-thunk'
-import { reducer,addNum, reduceNum,laterNum} from './index.redux' 
-const store = createStore(reducer,applyMiddleware(thunk));
+import { reducer,addNum, reduceNum,laterNum} from './index.redux';
+const reduxDevtools= window.devToolsExtension?window.devToolsExtension():f=>f
+const store = createStore(reducer,
+    compose(applyMiddleware(thunk),
+        reduxDevtools
+        ));
+
 
 const render= () =>{
     ReactDom.render(<App store={store} addNum={addNum} reduceNum={reduceNum} laterNum={laterNum}/>,document.getElementById('root'))
